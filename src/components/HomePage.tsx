483901639f407aebc6645f2189ceb5c56fa79bdc
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2024-12-20 02:03:59 +0530

    Created the create room card

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,37 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
-
 function HomePage() {
-
-  const userEmail = useSelector((state:RootState)=>state.user.email);
-
-  return <div>Home: {userEmail}</div>;
+  return (
+    <>
+      <div>
+        <div className="card flex items-center bg-black shadow-xl p-4">
+          <div className="flex-row card-body">
+            <button className="btn btn-secondary btn-sm">Create Room</button>
+            <button className="btn btn-ghost btn-sm">Join Room</button>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="label">
+              <span className="label-text">Enter your name:</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered input-sm w-full"
+            />
+            <div className="label">
+              <span className="label-text">Select topic:</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered input-sm w-full"
+            />
+            <div className="flex justify-center mt-4">
+              <button className="btn btn-secondary btn-sm">Create Room</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [ "night"],
   },
   plugins: [daisyui],
 }
